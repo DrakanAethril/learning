73b7f885df705fff13136e5360d4d181b655b016
Sébastien Tharaud <sebastien@iconosqua.re>
2022-12-06 18:28:25 +0100

    wip translations

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+
+    #[Route('/', name: 'homepage')]
+    public function indexNoLocale(): Response
+    {
+        return $this->redirectToRoute('home_lg', ['_locale' => 'fr']);
+    }
+
+    #[Route('/{_locale<%app.supported_locales%>}/', name: 'home_lg')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
