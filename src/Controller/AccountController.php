2b604138649d4368446f1f0c4dd6749088853b8f
Sébastien Tharaud <sebastien@iconosqua.re>
2022-12-23 09:39:33 +0100

    cohort editing

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,12 +4,15 @@ namespace App\Controller;
 
 use App\Form\ChangePasswordFormType;
 use App\Form\UserInfoUpdateFormType;
+use App\Repository\StructureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[Route('/me', name: 'account_')]
 class AccountController extends AbstractController
@@ -46,9 +49,40 @@ class AccountController extends AbstractController
     }
 
     #[Route('/structure', name: 'structure')]
-    public function structure(): Response
+    public function structure(Request $request, StructureRepository $structureRepository, EntityManagerInterface $entityManager, TranslatorInterface $translator): Response
     {
+        $user = $this->getUser();
+        $errors = [];
+        if($request->isMethod('POST') && !empty($request->request->get('structure')) ) {
+            $structureIdSaved = $request->request->get('structure');
+            $structureSaved = $structureRepository->findOneBy(['id' => $structureIdSaved]);
+            $cohortsSaved = $request->request->all()['cohort-'.$structureIdSaved] ?? [];
+            // does user have the structure ?
+            if(!$user->getStructures()->contains($structureSaved)) {
+                $errors[] = $translator->trans('account-errors.invalid-structure');
+            } else {
+                if(empty($cohortsSaved)) {
+                    $errors[] = $translator->trans('account-errors.empty-cohorts');
+                } else {
+                    // get structure potential cohorts
+                    $potentialCohorts = $structureSaved->getCohorts();
+                    if(!empty($potentialCohorts)) {
+                        foreach($potentialCohorts as $currentCohort) {
+                            if(in_array($currentCohort->getId(), $cohortsSaved)) { //Entity is already testing if it's conatined or not, no need to recheck here.
+                                $user->addCohort($currentCohort);
+                            } else {
+                                $user->removeCohort($currentCohort);
+                            }
+                        }
+                    }
+                    $entityManager->persist($user);
+                    $entityManager->flush();
+                }
+            }
+        }
+
         return $this->render('account/structure.html.twig', [
+            'formErrors' => $errors, 
             'accountTab' => 'structure'
         ]);
     }
